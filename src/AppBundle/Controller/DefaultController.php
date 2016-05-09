550e4f01cf6ac452afa4aeb83721195f761164b1
Rokas Milasevicius <rokas@milasevicius.com>
2016-05-09 16:11:38 +0300

    Keep out of symfony because its evil

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        //return $this->render('default/index.html.twig', array());
-        return $this->redirectToRoute('cv');
+        return $this->render('default/index.html.twig', array());
     }
 
     /**
