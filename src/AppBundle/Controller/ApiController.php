a5a2d6f30e1f79baf0501e247e80a52c82a2f5dc
Laurynas Mališauskas <laurynas.malisauskas@gmail.com>
2016-11-21 11:23:26 +0200

    bugfix

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Doctrine\ORM\ORMException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
