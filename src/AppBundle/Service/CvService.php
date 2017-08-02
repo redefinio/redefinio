1b0dc97eb8581d7180eded9e354a48db6b91ee5b
Oleg Sviderskij <oleg@dmarksai.com>
2017-08-02 22:49:21 +0300

    User cv page as home page

--- src/AppBundle/Service/CvService.php
+++ src/AppBundle/Service/CvService.php
@@ -2,6 +2,26 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * CVService
+ *
+ * @Service for manipulating cv's.
+ */
 class CvService {
 
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getUserCv(User $user) {
+        $userCV = $this->em->getRepository('AppBundle:CV')->findOneByUser($user);
+
+        return $userCV;
+    }
 }
